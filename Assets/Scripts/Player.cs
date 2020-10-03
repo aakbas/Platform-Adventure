d49f0d132f03e84bfecf79ab93cc9eb57b4216bd
Abdullah Akbaş <39574496+aakbas@users.noreply.github.com>
2020-10-03 20:46:37 +0300

    Player Movement and Animation state

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+
+    //Config
+    [SerializeField] float runSpeed = 5f;
+
+    //State
+    bool isAlive = true;
+    
+    //Cached
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Run();
+        FlipSprite();
         
     }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow*runSpeed,myRigidbody.velocity.y );
+        myRigidbody.velocity = playerVelocity;
+        UpdateAnimationState();
+    }
+
+    private void FlipSprite()
+    {
+        //if player is moving horizontaly  reverse the scaling of x axis
+        bool playerHasHorizontalspeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalspeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+        
+    }
+
+     private void UpdateAnimationState()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+
 }
