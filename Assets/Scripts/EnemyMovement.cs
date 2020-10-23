5d2114f0b5ab41e263816b164931ec470a0f88c4
Abdullah Akbaş <39574496+aakbas@users.noreply.github.com>
2020-10-23 20:01:32 +0300

    Enemy Movement (Redone)

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
     //Config Params
     [SerializeField] float moveSpeed = 1f;
+    [SerializeField] Player player;
+    [SerializeField] float chaseCooldown = 1f;
+    [SerializeField] float chaseStopTime = 1f;
 
     //States
-    bool isChasingPlayer = false;
+  [SerializeField]   bool isChasingPlayer = false;
+  [SerializeField]  bool isReachedBorder = false;
 
     //Cached Ref
     Rigidbody2D myRigidbody;
     CircleCollider2D myCircleCollider;
+    BoxCollider2D myBoxCollider;
     
 
 
@@ -21,29 +27,94 @@ public class EnemyMovement : MonoBehaviour
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myCircleCollider = GetComponent<CircleCollider2D>();
+        myBoxCollider = GetComponent<BoxCollider2D>();
         
     }
 
  
     void Update()
     {
-        if (!isChasingPlayer)
-        {
-            Move();
-        }
+       
+        LocatePositions();
+        FlipSprite();
+        Move();
         
     }
 
     private void Move()
     {
-        if(myCircleCollider.IsTouchingLayers(LayerMask.GetMask("Path Checkpoint")))
+        var moveTowards =transform.localScale.x;
+
+        if (!isReachedBorder && !isChasingPlayer)
         {
-            moveSpeed *= -1;
-            transform.localScale =new Vector2(transform.localScale.x*(-1),1f);
+            if (moveTowards > 0)
+            {
+                myRigidbody.velocity = new Vector2(-moveSpeed,0f);
+            }
+            else
+            {
+                myRigidbody.velocity = new Vector2(moveSpeed, 0f);
+            }        
         }
-        myRigidbody.velocity = new Vector2(-moveSpeed, 0f);
-        
+
+        if(!isReachedBorder && isChasingPlayer)
+        {
+            if (LocatePositions()<0)
+            {
+                myRigidbody.velocity = new Vector2(-moveSpeed, 0f);
+            }
+            else
+            {
+                myRigidbody.velocity = new Vector2(moveSpeed, 0f);
+            }
+        }   
+
+
+
     }
 
- 
+
+
+    
+
+    private float LocatePositions()
+    {
+        var playerPosX = player.transform.position.x;
+        var enemyPosX = gameObject.transform.position.x;
+        var deltaX = playerPosX - enemyPosX;
+        return deltaX;
+    }  
+
+    private void FlipSprite()
+    {
+        //if player is moving horizontaly  reverse the scaling of x axis
+        bool enemyHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (enemyHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(-(myRigidbody.velocity.x)), 1f);
+        }
+
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>())
+        {
+            isChasingPlayer = true;
+        }
+       
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>())
+        {
+            isChasingPlayer = false;
+        }
+    }
+
+
+
+
 }
