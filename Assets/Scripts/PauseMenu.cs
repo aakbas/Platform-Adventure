3440def90c8565e30e2a9615a4bd1e9279b1b5ec
Abdullah Akbaş <39574496+aakbas@users.noreply.github.com>
2021-01-08 00:51:27 +0300

    pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.EventSystems;
+
+public class PauseMenu : MonoBehaviour
+{
+    bool isPaused = false;
+    [SerializeField] GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isPaused)
+        {
+            if (CrossPlatformInputManager.GetButtonDown("Pause"))
+            {
+                isPaused = togglePause();
+                pauseMenu.SetActive(true);
+            }
+        }
+        else
+        {
+
+            if (CrossPlatformInputManager.GetButtonDown("Pause"))
+            {
+                isPaused = togglePause();
+                pauseMenu.SetActive(false);
+            }
+        }
+    }
+
+
+    bool togglePause()
+    {
+        if (Time.timeScale == 0f)
+        {
+            Time.timeScale = 1f;
+            return (false);
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            return (true);
+        }
+    }
+    public void Continue()
+    {
+        isPaused = togglePause();
+        pauseMenu.SetActive(false);
+    }
+
+
+
+}
